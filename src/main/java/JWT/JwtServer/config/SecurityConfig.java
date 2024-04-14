78a52db49824dbee6d365e29b1fc72ff545ca634
dayeong <dayeongyun184@gmail.com>
2024-04-14 16:49:05 +0900

    cors 설정

--- src/main/java/JWT/JwtServer/config/SecurityConfig.java
+++ src/main/java/JWT/JwtServer/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package JWT.JwtServer.config;
 import JWT.JwtServer.jwt.JWTFilter;
 import JWT.JwtServer.jwt.JWTUtil;
 import JWT.JwtServer.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +16,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -43,6 +48,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http
+                .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
         http
                 .csrf((auth)->auth.disable()); // 세션 방식으로 하면 세션이 고정되어 있기 때문에 csrf 공격을 방어해줘야됨
                                            // but 우린 Jwt 방식을 사용할거라 상관 없음
